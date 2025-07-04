1f5600b7881a0d2535cdb0476c1497b7eaeba984
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2025-07-04 17:07:47 +0300

    add subtitle file

--- api/constants/__init__.py
+++ api/constants/__init__.py
@@ -14,9 +14,9 @@ VIDEO_EXTENSIONS.extend([ext.upper() for ext in VIDEO_EXTENSIONS])
 AUDIO_EXTENSIONS = ["mp3", "m4a", "wav", "webm", "amr"]
 AUDIO_EXTENSIONS.extend([ext.upper() for ext in AUDIO_EXTENSIONS])
 
-
 if dify_config.ETL_TYPE == "Unstructured":
-    DOCUMENT_EXTENSIONS = ["txt", "markdown", "md", "mdx", "pdf", "html", "htm", "xlsx", "xls", "vtt", "properties"]
+    DOCUMENT_EXTENSIONS = ["txt", "str", "markdown", "md", "mdx", "pdf", "html", "htm", "xlsx", "xls", "vtt",
+                           "properties"]
     DOCUMENT_EXTENSIONS.extend(("doc", "docx", "csv", "eml", "msg", "pptx", "xml", "epub"))
     if dify_config.UNSTRUCTURED_API_URL:
         DOCUMENT_EXTENSIONS.append("ppt")
